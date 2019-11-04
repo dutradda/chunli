7cdcf1e66a074c951c75718509354690ffd2a598
Diogo Dutra <diogo.albuquerque@luizalabs.com>
2019-11-04 01:37:46 -0300

    Add json examples

--- docs/src/index/index_02_plugin_input_curl.bash
+++
@@ -1,3 +0,0 @@
-curl -X POST \
-    -i "localhost:8000/start?duration=2s&rps_per_node=100&uuid=uuid.uuid4" \
-    --upload-file=/tmp/hello-call.gz
--- docs/src/index/index_02_plugin_param_input.bash
+++
@@ -1,5 +0,0 @@
-echo \
-'{"url":"http://localhost:8000/hello","headers":{"x-uuid":"$uuid"}}' \
-    | gzip > /tmp/hello-call.gz
-
-zcat /tmp/hello-call.gz
